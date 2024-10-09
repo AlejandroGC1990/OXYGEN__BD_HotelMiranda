1d174576438da5cc746e1d788672a7616de03600
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-10-09 18:31:17 +0200

    edit roomRoutes

--- src/routes/roomRoutes.ts
+++ src/routes/roomRoutes.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import { createRoom, updateRoom, removeRoom, convertRoomToCSV, getRoomById, getAllRooms } from '../controllers/roomController';
+import { createRoom, updateRoom, removeRoom, getRoomById, getAllRooms } from '../controllers/roomController';
 import { verifyToken } from "../middleware/auth";
 
 const router = Router();
 
 router.get('/', verifyToken, getAllRooms);
 router.get('/:id', verifyToken, getRoomById);
-router.get('/csv', verifyToken, convertRoomToCSV);
 router.post('/', verifyToken, createRoom);
 router.put('/:id', verifyToken, updateRoom);
 router.delete('/:id', verifyToken, removeRoom);
