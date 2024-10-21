192a9d6a68ed3aa258e701c5169c9d28924a97ed
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-10-21 19:22:32 +0200

    change contactControllers.tsx from json to mongoDB

--- src/controllers/contactControllers.ts
+++ src/controllers/contactControllers.ts
@@ -1,90 +1,74 @@
 import { Request, Response } from "express";
-import { Contact } from "../interfaces/contact";
-import { getAll, getById, create, update, remove, convertJSONToCSV } from '../services/crud';
+import ContactModel from "../models/contactModels";
 
-//? Ruta al archivo JSON de contactos
-const contactsFilePath = '../data/contact.json';
-
-//? Obtener todos los contactos
-export const getAllContacts = (req: Request, res: Response): void => {
-    const contacts = getAll<Contact>(contactsFilePath);
-    res.status(200).json(contacts);
+//?? Obtener todos los contactos
+export const getAllContacts = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const contacts = await ContactModel.find();
+        res.status(200).json(contacts);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener los contactos', error });
+    }
 };
 
 //? Obtener contacto por ID
-export const getContactById = (req: Request, res: Response): void => {
-    const id = parseInt(req.params.id);
-    const contact = getById<Contact>(contactsFilePath, 'guest_idReview', id);
-
-    if (contact) {
-        res.status(200).json(contact);
-    } else {
-        res.status(404).json({ message: "Contacto no encontrado" });
+export const getContactById = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const contact = await ContactModel.findById(req.params.id);
+        if (contact) {
+            res.status(200).json(contact);
+        } else {
+            res.status(404).json({ message: "Contacto no encontrado" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener el contacto', error });
     }
 };
 
 //? Crear un nuevo contacto
-export const createContact = (req: Request, res: Response): void => {
-    const newContact: Contact = req.body;
+export const createContact = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const newContact = new ContactModel(req.body);
 
-    if (!newContact.guest_name || !newContact.guest_email) {
-        res.status(400).json({ message: 'El nombre y el email son requeridos' });
+        if (!newContact.guest_name || !newContact.guest_email) {
+            res.status(400).json({ message: 'El nombre y el email son requeridos' });
+            return;
+        }
+
+        //? Guardar el nuevo contacto automáticamente llamará al middleware para hashear la contraseña
+        await newContact.save();
+        res.status(201).json({ message: 'Usuario creado', contact: newContact });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al crear el usuario', error });
     }
-
-    const createdContact = create<Contact>(contactsFilePath, newContact, 'guest_idReview');
-    res.status(201).json({ message: 'Contacto creado', contact: createdContact });
 };
 
 //? Actualizar un contacto existente
-export const updateContact = (req: Request, res: Response): void => {
-    const id = parseInt(req.params.id);
-    const updatedData: Partial<Contact> = req.body;
+export const updateContact = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const updatedContact = await ContactModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
-    if (!updatedData.guest_name && !updatedData.guest_email) {
-        res.status(400).json({ message: 'Proporcione al menos un nombre o un email para actualizar' });
-    }
-
-    const updatedContact = update<Contact>(contactsFilePath, 'guest_idReview', id, updatedData);
-
-    if (updatedContact) {
-        res.status(200).json({ message: 'Contacto actualizado', contact: updatedContact });
-    } else {
-        res.status(404).json({ message: 'Contacto no encontrado' });
+        if (updatedContact) {
+            res.status(200).json({ message: 'Contacto actualizado', contact: updatedContact });
+        } else {
+            res.status(404).json({ message: 'Contacto no encontrado' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar el usuario', error });
     }
 };
 
 //? Eliminar un contacto
-export const removeContact = (req: Request, res: Response): void => {
-    const id = parseInt(req.params.id);
-    const success = remove<Contact>(contactsFilePath, 'guest_idReview', id);
+export const removeContact = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const success = await ContactModel.findByIdAndDelete(req.params.id);
 
-    if (success) {
-        res.status(200).json({ message: 'Contacto eliminado' });
-    } else {
-        res.status(404).json({ message: 'Contacto no encontrado' });
-    }
-};
-
-//? Convertir contactos a CSV
-export const convertContactsToCSV = (req: Request, res: Response) => {
-    const fileName = 'contacts.json';
-    const headers = [
-        'guest_idReview',
-        'guest_timeDateReview',
-        'guest_DateReview',
-        'guest_name',
-        'guest_email',
-        'guest_phone',
-        'guest_rateReview',
-        'guest_commentReview',
-        'guest_statusReview',
-        'guest_checkIn',
-        'guest_checkInTime',
-        'guest_checkOut',
-        'guest_checkOutTime',
-        'guest_orderDateTime',
-        'guest_orderDate',
-        'guest_room_state'
-    ];
-    convertJSONToCSV(req, res, fileName, headers);
+        if (success) {
+            res.status(200).json({ message: 'Contacto eliminado' });
+        } else {
+            res.status(404).json({ message: 'Contacto no encontrado' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar el usuario', error });
+    };
 };
